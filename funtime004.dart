3c7d297cec5130797bf3bc2b567e54365f08a467
banjo254 <benjaminmariga@gmail.com>
2023-02-13 23:13:35 +0300

    request user input

---
+++ funtime004.dart
@@ -0,0 +1,12 @@
+import 'dart:io';
+
+void main() {
+  int currentYear = 2023;
+  print("Enter your birth date:");
+  int? birthyear = int.parse(stdin.readLineSync()!);
+  if ((currentYear - birthyear) > 18) {
+    print("You are an adult");
+  } else {
+    print("You are a minor");
+  }
+}
