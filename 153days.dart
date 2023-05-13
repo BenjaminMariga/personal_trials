cc9cd2be900cbbe63a60114d6e1309d5664831f2
banjo254 <benjaminmariga@gmail.com>
2023-05-13 20:08:28 +0300

    153 days later,,,we back

---
+++ 153days.dart
@@ -0,0 +1,22 @@
+//strings
+void main() {
+  var sms = "new car";
+  print(sms.length);
+  var front = "this is";
+  var mid = "the correct answer:";
+  var answer = front + ' ' + mid;
+  var meeting = answer.substring(0, 4);
+  print(answer);
+  print(meeting);
+
+  bool isRequired = false;
+  bool isValid = false;
+  bool result = isRequired || isValid;
+
+  bool isActive = true;
+  bool isPasswordCorrect = true;
+  bool isAuthenticated = isActive && isPasswordCorrect;
+  print(isAuthenticated);
+
+  print(result);
+}
